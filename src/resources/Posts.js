15d9d1261bb7dcec6f94f3c1f355cc7798d7e0ad
Karl Taylor <Karl@workshop.ws>
2018-08-29 14:12:58 +0100

    fixed posts

--- src/resources/Posts.js
+++ src/resources/Posts.js
@@ -1,41 +1,44 @@
 export default function Posts(discourse) {
+  this.create = ({ api_username, topic_id, raw }) => {
+    return new Promise((resolve, reject) => {
+      if (!api_username)
+        return reject(
+          new Error("No api_username defined. You must pass a username to the create function.")
+        );
+      if (!topic_id)
+        return reject(new Error("No topic_id defined. You must pass a topic to create function."));
 
-  this.create = (username, topic_id, raw) => new Promise((resolve, reject) => {
-    if (!username) return reject(new Error('No username defined. You must pass a topic to create function.'));
-    if (!topic_id) return reject(new Error('No topic_id defined. You must pass a topic to create function.'));
+      var form = new FormData();
+      form.append("api_key", discourse._API_KEY);
+      form.append("api_username", api_username);
+      form.append("topic_id", topic_id.toString());
+      form.append("raw", raw);
 
-    fetch(`${discourse._BASE_URL}/posts?${discourse._API_KEY}`, {
-      method: 'post',
-      headers: {
-        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-      },
-      body: {
-        username,
-        topic_id,
-        raw
-      }
-    })
-    .then(response => {
-      if (response.ok) {
-        return response.json()
-      } else {
-        throw new Error(response.statusText, response.status);
-      }
-    })
-    .then(function (data) {
-      resolve(data)
-    })
-    .catch(function (error) {
-      reject(error)
+      return fetch(`${discourse._BASE_URL}/posts`, {
+        method: "POST",
+        mimeType: "multipart/form-data",
+        body: form
+      })
+        .then(response => {
+          if (response.ok) {
+            return resolve(response.json());
+          } else {
+            return reject(new Error(response.statusText, response.status));
+          }
+        })
+        .catch(function(error) {
+          if (error) {
+            return reject(new Error(error));
+          }
+        });
     });
-  })
+  };
 
-  this.reply = () => new Promise((resolve, reject) => {
-    console.log('Reply')
-    resolve('Reply')
-  })
+  this.reply = () =>
+    new Promise((resolve, reject) => {
+      console.log("Reply");
+      resolve("Reply");
+    });
 
-  this.like = () => new Promise((resolve, reject) => {
-
-  })
+  this.like = () => new Promise((resolve, reject) => {});
 }
