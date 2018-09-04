94f153b1877bfdec87d7a65a7cc98408487d6cc4
Karl Taylor <Karl@workshop.ws>
2018-09-04 09:51:20 +0100

    add error for no id

--- src/resources/Topics.js
+++ src/resources/Topics.js
@@ -1,6 +1,10 @@
 export default function Topics(discourse) {
-  this.getTopic = ({ id }) => {
+  this.getTopic = ({ id } = {}) => {
     return new Promise((resolve, reject) => {
+      if (!id) return reject(
+        new Error("No id defined. You must pass an id to the getTopic function.")
+      );
+
       discourse
         .DiscourseResource({
           path: `t/${id}.json`,
