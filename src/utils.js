5dfd69ca3fd73fdc2ba65ea7888bd4f3b1135beb
Karl Taylor <Karl@workshop.ws>
2018-10-02 16:32:23 +0100

    Ran Prettier, updated eslintrc.json, removed from createBody util, updated README.md

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,3 @@
-// Create body takes in an objects
-// and then maps the object keys:values to FormData.
 export function createBody() {
   const form = new FormData();
   const params = arguments[0];
@@ -7,9 +5,6 @@ export function createBody() {
     const paramValue = params[paramKey];
     form.append(paramKey, paramValue);
   });
-  // for (var pair of form.entries()) {
-  //   console.log(pair[0] + ", " + pair[1]);
-  // }
   return form;
 }
 export class ApiError extends Error {
