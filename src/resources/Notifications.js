785635fa9d093aa227c801ee2f2989f055d0f117
Dan <dan@sortedfood.com>
2018-12-04 18:24:48 +0000

    Add ability to get notifications, and mark individual notifications as read

---
+++ src/resources/Notifications.js
@@ -0,0 +1,36 @@
+import { buildQueryString } from '../utils';
+
+export default function Notifications(discourse) {
+  this.get = (inputs = {}) => {
+    return new Promise((resolve, reject) => {
+      const params = {
+        api_key: discourse._API_KEY,
+        api_username: discourse._API_USERNAME,
+        ...inputs,
+      };
+
+      discourse
+        .DiscourseResource({
+          method: 'GET',
+          path: buildQueryString('notifications.json', params),
+        })
+        .then(response => resolve(response))
+        .catch(error => reject(error));
+    });
+  };
+
+  this.markRead = ({ id }) => {
+    return new Promise((resolve, reject) => {
+      discourse
+        .DiscourseResource({
+          method: "PUT",
+          path: 'notifications/mark-read',
+          body: {
+            id,
+          }
+        })
+        .then(response => resolve(response))
+        .catch(error => reject(error));
+    });
+  };
+}
--- src/utils.js
+++ src/utils.js
@@ -14,13 +14,22 @@ export function createBody() {
   });
   return form;
 }
+
+export function buildQueryString(uri, params) {
+  const queryString = Object.keys(params)
+    .map(key => key + '=' + params[key])
+    .join('&');
+  const separator = uri.indexOf('?') !== -1 ? '&' : '?';
+  return `${uri}${separator}${queryString}`;
+}
+
 export class ApiError extends Error {
-  constructor(status, statusText, error, erorrArray = []) {
+  constructor(status, statusText, error, errorArray = []) {
     super();
     this.name = 'ApiError';
     this.status = status;
     this.statusText = statusText;
     this.message = `${status} - ${statusText || error}`;
-    this.error = error || erorrArray.join(', ');
+    this.error = error || errorArray.join(', ');
   }
 }
